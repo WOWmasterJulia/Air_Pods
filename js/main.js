91d1ee9fdc442cf19b2ee2e4fb2efc7dacd8dfe0
WOWmasterJulia <topazzz@ukr.net>
2023-10-03 12:21:57 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const chooseColor = document.querySelectorAll('.choose__color-btn');
+const contentItem = document.querySelectorAll('.content-item');
+
+chooseColor.forEach(function(element){
+   element.addEventListener('click', open) 
+})
+
+function open(evt){
+    const target = evt.currentTarget;
+
+    chooseColor.forEach(function(item){
+        item.classList.remove('choose__color-btn--active')
+    })
+    target.classList.add('choose__color-btn--active')
+}
